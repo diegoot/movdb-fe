db709a817d8e927c9a813906b92bf16b5031d35c
Diego Toranzo <dstoranzo@gmail.com>
2019-06-30 01:15:19 -0300

    Making footer responsive

--- src/components/AppFooter/AppFooter.js
+++ src/components/AppFooter/AppFooter.js
@@ -1,38 +1,46 @@
 import React from 'react'
-import { Icon } from 'antd'
+import { Icon, Row, Col } from 'antd'
 
 import './styles.css'
 
 const AppFooter = () => {
   return (
-    <div className="app-footer">
-      <ul>
-        <li>Who we are?</li>
-        <li>Join us</li>
-      </ul>
-      <ul>
-        <li>FAQ</li>
-        <li>Press</li>
-      </ul>
-      <ul>
-        <li>Legal terms</li>
-        <li>Contact us</li>
-      </ul>
-      <ul className="social">
-        <li>
-          <Icon type="twitter" className="footer-icon" />
-        </li>
-        <li>
-          <Icon type="linkedin" theme="filled" className="footer-icon" />
-        </li>
-        <li>
-          <Icon type="facebook" theme="filled" className="footer-icon" />
-        </li>
-        <li>
-          <Icon type="instagram" theme="filled" className="footer-icon" />
-        </li>
-      </ul>
-    </div>
+    <Row className="app-footer">
+      <Col lg={6}>
+        <ul>
+          <li>Who we are?</li>
+          <li>Join us</li>
+        </ul>
+      </Col>
+      <Col lg={6}>
+        <ul>
+          <li>FAQ</li>
+          <li>Press</li>
+        </ul>
+      </Col>
+      <Col lg={6}>
+        <ul>
+          <li>Legal terms</li>
+          <li>Contact us</li>
+        </ul>
+      </Col>
+      <Col lg={6}>
+        <ul className="social">
+          <li>
+            <Icon type="twitter" className="footer-icon" />
+          </li>
+          <li>
+            <Icon type="linkedin" theme="filled" className="footer-icon" />
+          </li>
+          <li>
+            <Icon type="facebook" theme="filled" className="footer-icon" />
+          </li>
+          <li>
+            <Icon type="instagram" theme="filled" className="footer-icon" />
+          </li>
+        </ul>
+      </Col>
+    </Row>
   )
 }
 

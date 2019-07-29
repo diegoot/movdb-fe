cbddfa1b84363f67d86766950d37df2ffead7181
Diego Toranzo <dstoranzo@gmail.com>
2019-07-29 00:26:35 -0300

    Adding go back to dashboard link in 404 page

--- src/components/pages/NotFound/NotFound.js
+++ src/components/pages/NotFound/NotFound.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Layout, Icon } from 'antd'
+import { Link } from 'react-router-dom'
 
 import './styles.css'
 
@@ -12,6 +13,9 @@ const NotFound = () => {
         4<Icon type="frown" theme="twoTone" />4
       </div>
       <div className="message">Oops ! Nothing found here</div>
+      <div>
+        Go back to <Link to="/">Dashboard</Link>
+      </div>
     </Content>
   )
 }

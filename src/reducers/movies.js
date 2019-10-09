f500786735e91dd9ce35a591b106c60dad63d861
Diego Toranzo <dstoranzo@gmail.com>
2019-10-09 00:00:53 -0300

    Refactor movies reducer

--- src/reducers/movies.js
+++ src/reducers/movies.js
@@ -3,35 +3,20 @@ import * as actionTypes from '../constants/actionsTypes/movies'
 export default (state = {}, action) => {
   switch (action.type) {
     case actionTypes.MOST_N_RECENT_MOVIES_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isError: false,
-        list: []
-      })
-    case actionTypes.MOST_N_RECENT_MOVIES_SUCCESS:
-      return Object.assign({}, state, {
-        isFetching: false,
-        error: false,
-        list: action.payload
-      })
-    case actionTypes.MOST_N_RECENT_MOVIES_FAILURE:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isError: true,
-        list: []
-      })
     case actionTypes.MOVIES_FOR_GENRE_REQUEST:
       return Object.assign({}, state, {
         isFetching: true,
         isError: false,
         list: []
       })
+    case actionTypes.MOST_N_RECENT_MOVIES_SUCCESS:
     case actionTypes.MOVIES_FOR_GENRE_SUCCESS:
       return Object.assign({}, state, {
         isFetching: false,
         error: false,
         list: action.payload
       })
+    case actionTypes.MOST_N_RECENT_MOVIES_FAILURE:
     case actionTypes.MOVIES_FOR_GENRE_FAILURE:
       return Object.assign({}, state, {
         isFetching: false,
@@ -39,6 +24,7 @@ export default (state = {}, action) => {
         list: []
       })
     case actionTypes.CREATE_MOVIE_REQUEST:
+    case actionTypes.DELETE_MOVIE_REQUEST:
       return Object.assign({}, state, {
         isFetching: true,
         isError: false
@@ -49,25 +35,16 @@ export default (state = {}, action) => {
         error: false
       })
     case actionTypes.CREATE_MOVIE_FAILURE:
+    case actionTypes.DELETE_MOVIE_FAILURE:
       return Object.assign({}, state, {
         isFetching: false,
         isError: true
-      })
-    case actionTypes.DELETE_MOVIE_REQUEST:
-      return Object.assign({}, state, {
-        isFetching: true,
-        isError: false
       })
     case actionTypes.DELETE_MOVIE_SUCCESS:
       return Object.assign({}, state, {
         isFetching: false,
         error: false,
         list: state.list.filter(movie => movie._id !== action.payload)
-      })
-    case actionTypes.DELETE_MOVIE_FAILURE:
-      return Object.assign({}, state, {
-        isFetching: false,
-        isError: true
       })
     default:
       return state
